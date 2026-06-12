542d6935ebac3ffe9fbdb2f702f0966c6165166b
LucianoASF <lucianoaugustos12@gmail.com>
2026-06-11 21:49:25 -0300

    feat: add navbar

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+    <div class="container-fluid">
+      <router-link class="navbar-brand fw-bold" to="/">
+        HelpDesk
+      </router-link>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbar"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">
+              Dashboard
+            </router-link>
+          </li>
+
+          <li class="nav-item">
+            <router-link class="nav-link" to="/tickets">
+              Chamados
+            </router-link>
+          </li>
+
+          <li class="nav-item">
+            <router-link class="nav-link" to="/tickets/new">
+              Novo Chamado
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
