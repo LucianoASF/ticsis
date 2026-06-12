a3d749b347ac3121b126fe5161a5e3d65bba8137
LucianoASF <lucianoaugustos12@gmail.com>
2026-06-12 17:55:38 -0300

    chore: add seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+users = 10.times.map do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email,
+  )
+end
+
+20.times do
+  Ticket.create!(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    status: %w[OPEN IN_PROGRESS RESOLVED CLOSED].sample,
+    priority: %w[LOW MEDIUM HIGH CRITICAL].sample,
+    user: users.sample
+  )
+end
